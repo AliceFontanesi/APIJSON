f5c20e6ba857d30aaf5bcde53f7834814cec4edc
AliceFontanesi <fontanesi.alice@iisviolamarchesini.edu.it>
2024-02-14 23:27:21 +0100

    create da sistemare

--- apijson.py
+++ apijson.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, url_for
 from product import Product
 
 app = Flask(__name__)
@@ -25,7 +25,41 @@ def get_product(product_id):
     keys = ["id", "nome", "prezzo", "marca"]
     product_dict = {key: value for key, value in zip(keys, record)}
     return jsonify(product_dict)
+
+
+
+
+
+
+
+@app.route('/products', methods=['POST'])
+def create_product():
+    # Controlla se la richiesta è in formato JSON
+    if not request.json:
+        return jsonify({'error': 'Richiesta deve essere in formato JSON'}), 400
     
+    # Estrai i dati dal corpo della richiesta JSON
+    data = request.json.get('data')
+    if not data:
+        return jsonify({'error': 'Dati non forniti'}), 400
+    
+    # Estrai i dati specifici del prodotto
+    product_data = data.get('attributes')
+    if not product_data:
+        return jsonify({'error': 'Attributi del prodotto non forniti'}), 400
+    
+    # Creazione del nuovo prodotto
+    new_product = {
+        #'id': 
+        'marca': product_data.get('marca'),
+        'nome': product_data.get('nome'),
+        'prezzo': product_data.get('prezzo')
+    }
+    
+    return new_product
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
